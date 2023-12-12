bc1e204c9022f3c55e1a1d5ddf7fe7744fb0cf4b
gargkartik1202 <gargkartik1202@gmail.com>
2023-12-12 14:15:12 +0530

    sdcs

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void solve(){
+    int n;
+    cin >> n;
+    int count=0;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+    cin>>a[i];
+    }
+}
+
+signed main(){
+    
+    int tc = 1;
+    cin>>tc; 
+    while(tc--){
+        solve();
+        cout << endl;
+}
+    return 0;
+}
